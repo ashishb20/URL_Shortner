37f6b5949497033b40094efe68265585837f29f0
Ashish Bairwa <ashish.jdh2020@gmail.com>
2026-01-30 06:34:43 +0530

    databse schema created

---
+++ backend/src/models/url.model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+const urlSchema = new mongoose.urlSchema(
+    {
+        longUrl: {
+            type: String,
+            required: true
+        },
+        shortCode: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        clicks: {
+            type: Number,
+            default: 0
+        }
+    },
+    {
+        timestamps: { createdAt: true, updatedAt: true }
+    }
+);
+
+export default mongoose.model('Url', urlSchema);
