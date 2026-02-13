d743c6f4bbbb4f5de370dd474d13ee986eb2188e
Ashish Bairwa <ashish.jdh2020@gmail.com>
2026-02-14 00:55:28 +0530

    cors issue resolve

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,15 @@ import cors from 'cors';
 import dotenv from "dotenv";
 import connectDB from './config/db.js';
 import app from './app.js';
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 connectDB();
 
 //  for deployment
-const path = require("path");
-
+import path from "path";
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.use((req, res) => {
