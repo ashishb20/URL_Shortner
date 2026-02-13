9a0be69505dfeb69611e49f25b2d48fda7627c58
Ashish Bairwa <ashish.jdh2020@gmail.com>
2026-02-14 00:26:08 +0530

    Ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,15 @@ import app from './app.js';
 dotenv.config();
 connectDB();
 
+//  for deployment
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
