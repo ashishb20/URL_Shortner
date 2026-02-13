583094c360e4b5102f9f15ae3ec73c888564cc93
Ashish Bairwa <ashish.jdh2020@gmail.com>
2026-02-14 00:58:53 +0530

    cors issue resolve

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from './config/db.js';
 import app from './app.js';
 import { fileURLToPath } from "url";
+import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
